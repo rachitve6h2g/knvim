73ec9baef946aae84704ef59b8997ee25e7f59bd
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-11-23 02:30:51 +0530

    stylua: formatted stuff

--- nvim/plugin/neorg.lua
+++ nvim/plugin/neorg.lua
@@ -1,24 +1,24 @@
 require("neorg").setup({
-	load = {
-		["core.defaults"] = {},
-		["core.concealer"] = {},
-		["core.dirman"] = {
-			config = {
-				workspaces = {
-					notes = "~/Documents/Notes",
-				},
-				default_workspace = "notes",
-			},
-		},
-		["core.integrations.telescope"] = {
-			config = {
-				insert_file_link = {
-					show_title_preview = true,
-				},
-			},
-		},
-		["core.integrations.treesitter"] = {},
-	},
+  load = {
+    ["core.defaults"] = {},
+    ["core.concealer"] = {},
+    ["core.dirman"] = {
+      config = {
+        workspaces = {
+          notes = "~/Documents/Notes",
+        },
+        default_workspace = "notes",
+      },
+    },
+    ["core.integrations.telescope"] = {
+      config = {
+        insert_file_link = {
+          show_title_preview = true,
+        },
+      },
+    },
+    ["core.integrations.treesitter"] = {},
+  },
 })
 
 vim.wo.foldlevel = 99
